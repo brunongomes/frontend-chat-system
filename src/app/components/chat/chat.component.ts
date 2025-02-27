07d83650887129367309af8baadcca2eb373e532
Bruno Gomes <brunongomes8@gmail.com>
2025-02-26 22:26:41 -0300

    feat: add chat component

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { WebsocketService } from '../../services/websocket.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-chat',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.css'
+})
+export class ChatComponent {
+  messages: any[] = [];
+  newMessage = '';
+  subscription!: Subscription;
+
+  constructor(
+    private websocketService: WebsocketService,
+    private router: Router
+  ) {
+    this.websocketService.connect().subscribe((connected: any) => {
+      if (connected) {
+        this.websocketService.fetchMessages().subscribe();
+        this.subscription = this.websocketService.listenMessages().subscribe(
+          (message: any) => this.messages.push(message)
+        );
+      }
+    });
+  }
+
+  sendMessage() {
+    const responseWebsocket = this.websocketService.sendMessage(this.newMessage);
+    if (!responseWebsocket) {
+      alert('Você precisa estar logado para enviar mensagens!');
+      this.router.navigate(['/login']);
+    }
+    this.newMessage = '';
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
