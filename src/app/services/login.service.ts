3cdd555b1fc4670ba3b06c995412875310ad99c0
Bruno Gomes <brunongomes8@gmail.com>
2025-02-26 22:28:13 -0300

    feat: add login service

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  private apiUrl = 'http://localhost:9007/auth/login';
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: { username: string, password: string }): Observable<string> {
+    return this.http.post<string>(this.apiUrl, credentials);
+  }
+}
