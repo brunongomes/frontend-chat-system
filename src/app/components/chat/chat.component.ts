8c7f8d12a59ebf867d22ec1829680025952a7e37
Bruno Gomes <brunongomes8@gmail.com>
2025-02-27 15:43:00 -0300

    refactor: processing optimization

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, TrackByFunction } from '@angular/core';
 import { WebsocketService } from '../../services/websocket.service';
 import { Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
@@ -16,42 +16,70 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
-export class ChatComponent {
+export class ChatComponent implements OnDestroy {
   messages: any[] = [];
   newMessage = '';
   subscription!: Subscription;
+  trackMessage: TrackByFunction<any> = (index, message) => message.id;
+  loading = false;
 
   constructor(
     private websocketService: WebsocketService,
     private router: Router,
     private authService: AuthService
   ) {
-    this.websocketService.connect().subscribe((connected: any) => {
-      if (connected) {
-        this.websocketService.fetchMessages().subscribe();
-        this.subscription = this.websocketService.listenMessages().subscribe(
-          (message: any) => this.messages.push(message)
-        );
+    this.initWebSocket();
+  }
+
+  private initWebSocket() {
+    this.websocketService.connect().subscribe({
+      next: (value: unknown) => {
+        const connected = value as boolean;
+        if (connected) {
+          this.loadMessages();
+          this.setupMessageListener();
+        }
+      }
+    });
+  }
+
+  private loadMessages() {
+    this.websocketService.fetchMessages().subscribe({
+      next: () => {
+        this.messages = this.websocketService.messages;
+      }
+    });
+  }
+
+  private setupMessageListener() {
+    this.subscription = this.websocketService.listenMessages().subscribe({
+      next: (message: any) => {
+        this.messages = [...this.messages, message];
       }
     });
   }
 
   sendMessage() {
-    const responseWebsocket = this.websocketService.sendMessage(this.newMessage);
-    if (!responseWebsocket) {
-      alert('Você precisa estar logado para enviar mensagens!');
+    if (!this.authService.isLoggedIn()) {
+      alert('Você precisa estar logado!');
       this.router.navigate(['/login']);
+      return;
     }
-    this.newMessage = '';
+
+    this.websocketService.sendMessage(this.newMessage).subscribe({
+      next: () => {
+        this.newMessage = '';
+      }
+    });
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.subscription?.unsubscribe();
+    this.websocketService.socket?.disconnect();
   }
 
   logout() {
-    this.authService.removeToken('authToken');
-    this.authService.removeToken('userId');
+    this.authService.logout();
     this.router.navigate(['/login']);
   }
 }
