72225fa2101799bcccf0af874a24c937d79e80bf
Bruno Gomes <brunongomes8@gmail.com>
2025-02-27 21:49:53 -0300

    refactor: update environments

--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,7 @@ const port = 9007;
 
 export const environment = {
     production: false,
-    apiUrl: `http://localhost:${port}`,
+    apiUrl: 'https://backend-chat-system-tq32.vercel.app',
     port: port,
-    wsUrl: `ws://localhost:${port}`
+    wsUrl: 'wss://backend-chat-system-tq32.vercel.app'
 };
